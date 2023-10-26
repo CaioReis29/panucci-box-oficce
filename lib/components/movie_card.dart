15d1f461c9aefd1b41e575a9003760550bcf8655
CaioReis29 <114621145+CaioReis29@users.noreply.github.com>
2023-10-26 15:49:26 -0300

    feat: movies listed on the home screen using bloc

--- lib/components/movie_card.dart
+++ lib/components/movie_card.dart
@@ -9,41 +9,36 @@ class MovieCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Card(
-            clipBehavior: Clip.hardEdge,
-            child: ConstrainedBox(
-              constraints: const BoxConstraints(minHeight: 200),
-              child: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => MovieScreen(movie: movie),
-                      ),
-                    );
-                  },
-                  child: movie.imageURI != null
-                      ? Ink(
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: NetworkImage(movie.imageURI!),
-                                fit: BoxFit.cover),
-                          ),
-                        )
-                      : Ink(
-                          child: const Center(
-                            child: Icon(Icons.error),
-                          ),
-                        )),
-            )),
-        Text(
-          movie.name,
-          style: Theme.of(context).textTheme.displaySmall,
-        )
-      ],
+    return SingleChildScrollView(
+      physics: const NeverScrollableScrollPhysics(),
+      child: Column(
+        children: [
+          InkWell(
+            child: Card(
+              elevation: 6,
+              clipBehavior: Clip.hardEdge,
+              child: movie.imageURI != null
+                  ? Image.network(
+                      movie.imageURI.toString(),
+                      fit: BoxFit.cover,
+                    )
+                  : const Center(
+                      child: Icon(Icons.error),
+                    ),
+            ),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MovieScreen(movie: movie),
+              ),
+            ),
+          ),
+          Text(
+            movie.name,
+            style: Theme.of(context).textTheme.displaySmall,
+          )
+        ],
+      ),
     );
   }
 }
